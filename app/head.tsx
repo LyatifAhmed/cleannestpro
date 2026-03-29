e44bf2f07f6f6e9831aa2d5c9b203b200db0baed
LyatifAhmed <latifmutlu@gmail.com>
2026-03-30 00:03:48 +0100

    feat: add iOS splash screens for PWA experience

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,38 @@ export default function Head() {
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="CleanNestPro" />
-      <meta name="mobile-web-app-capable" content="yes" />
+
+      {/* Splash screens */}
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-2048x2732.png"
+        media="(device-width: 1024px) and (device-height: 1366px)"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-1668x2388.png"
+        media="(device-width: 834px) and (device-height: 1194px)"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-1536x2048.png"
+        media="(device-width: 768px) and (device-height: 1024px)"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-1125x2436.png"
+        media="(device-width: 375px) and (device-height: 812px)"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-1170x2532.png"
+        media="(device-width: 390px) and (device-height: 844px)"
+      />
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-1284x2778.png"
+        media="(device-width: 428px) and (device-height: 926px)"
+      />
     </>
   );
 }
